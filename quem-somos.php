e8a7c7cee0c6ab3373178dd070927822b870ce97
gabrieldia4s <80932667+gabrieldia4s@users.noreply.github.com>
2022-01-24 01:58:52 -0300

    Add files via upload

---
+++ quem-somos.php
@@ -0,0 +1,163 @@
+<!doctype html>
+  <html lang="pt-br">
+  <head>
+    <title>QUEM SOMOS</title>
+    <link href="https://fonts.googleapis.com/css?family=Viga:400" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/signin.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" id="logo" href="index.php">IF MATH</a>
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                  <a class="nav-link active" id="guia1" aria-current="page" href="quem-somos.php">QUEM SOMOS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" id="guia2" href="#">CONTATO</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+      <div class="d-flex justify-content-center h-100">
+        <div class="card" id="capa"><br>
+            <div class="text-center">
+                <h4>QUEM SOMOS</h4>
+                <p class="texto">Somos alunos do quarto ano do curso de Informática do IFRN-Instituto Federal de Educação, Ciência e Tecnologia do Rio Grande do Norte.
+                    E desenvolvedores do IFMTH, que tem como objetivo a disponibilidade de uma plataforma de estudos para a disciplina de Matemática voltada para aprendizagem dos estudantes matriculados.
+                </p>
+            </div><br>
+            <div class="row justify-content-center texto card-group">
+                <div class="border-warning mb-3 bg-light text-dark col-3" style="max-width: 18rem;">
+                    <div class="card-header border-success text-center">Gabriel Klebson da Silva Dias</div>
+                    <div class="card-body text-success">
+                        <img src="img/gabriel.jpg" class="w-50 justify-content-end" alt="...">
+                    </div>
+                    <div class="card-footer border-success text-center" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#gabriel"><a href="#">Ver Mais</a></div>
+                </div>
+                <div class="border-warning mb-3 bg-light text-dark col-3" style="max-width: 18rem;">
+                    <div class="card-header border-success text-center">Stephany B. Borges Pereira</div>
+                    <div class="card-body text-success">
+                        <img src="img/stephany.jpg" class="w-50 justify-content-end" alt="...">
+                    </div>
+                    <div class="card-footer border-success text-center" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#stephany"><a href="#">Ver Mais</a></div>
+                </div>
+                <div class="border-warning mb-3 bg-light text-dark col-3" style="max-width: 18rem;">
+                    <div class="card-header border-success text-center">José Pedro Ribeiro Gomes</div>
+                    <div class="card-body text-success">
+                      <img src="img/pedro.jpg" class="w-50 justify-content-end" alt="...">
+                    </div>
+                    <div class="card-footer border-success text-center" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pedro"><a href="#">Ver Mais</a></div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" id="gabriel" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg"">
+          <div class="modal-content">
+            <div class="modal-header text-center">
+              <h5 class="modal-title" id="staticBackdropLabel">Gabriel Klebson da Silva Dias</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body row">
+                <div class="col-6">
+                    <img src="img/gabriel.jpg" class="w-100 justify-content-end" alt="...">
+                </div>
+                <div class="col-6 resumo">
+                  <p><b>RESUMO: </b> <small>Gabriel tem 19 anos e é aluno do 4° ano de informática do curso técnico em
+                    informática no IFRN, campus Ceará - Mirim, ele teve a proposta inicial junto com a
+                    professora de Matemática Danielle Nunes e é uma das mentes que elaboraram a
+                    plataforma IFmath do papel até os dias atuais, juntamente com seu grupo de amigos.
+                    "Sempre tive dificuldade em Matemática e exatas, por diversos fatores, inclusive a
+                    desigualdade escolar que é tão pertinente em nosso cotidiano. Projetamos esses
+                    empasses como pauta central da plataforma, sendo assim um apoio para essas
+                    pessoas que muita das vezes se sentem desnorteadas e desestimuladas, assim como
+                    eu me sentia".</small></p>
+                  <p><b>CURSO: </b> <small>Técnico em informática.</small></p>
+                  <p><b>CONTATO: </b> <small>(84)9 9424-6146</small></p>
+                  <p><b>E-MAIL: </b> <small>gabriel.diaz.k0@gmail.com</small></p>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal fade" id="stephany" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg"">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">Stephany Beatriz Borges Pereira</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body row">
+                <div class="col-6">
+                    <img src="img/stephany.jpg" class="w-100 justify-content-end" alt="...">
+                </div>
+                <div class="col-6 resumo">
+                  <p><b>RESUMO: </b> <small>Stephany Beatriz é aluna do 4° ano do curso técnico em informática do IFRN,
+                    Campus Ceará - Mirim, e é uma das mentes brilhantes que projetaram o IFMath desde
+                    seu princípio. Tem 18 anos, ama programar e mesmo diante das dificuldades
+                    corriqueiras nunca desistiu de tentar, mostrando que muita das vezes devemos sair
+                    de nossa zona de conforto para assim conquistarmos a experiência necessária para
+                    esse mundo tão desafiador.</small></p>
+                  <p><b>CURSO: </b> <small>Técnica em informática.</small></p>
+                  <p><b>CONTATO: </b> <small>(84) 98872-9149</small></p>
+                  <p><b>E-MAIL: </b> <small>stephanyborg3s@gmail.com</small></p>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal fade" id="pedro" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg"">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="staticBackdropLabel">José Pedro Ribeiro Gomes</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body row">
+                <div class="col-6">
+                    <img src="img/pedro.jpg" class="w-100 justify-content-end" alt="...">
+                </div>
+                <div class="col-6 resumo">
+                  <p><b>RESUMO: </b> <small>José Pedro é discente do 4° ano de informática, também no campus Ceará - Mirim,
+                    um dos contribuinte na idealização da plataforma, seus hobbys cotidianos são
+                    totalmente divergentes a matemática e programação, mas ele se propôs a entrar
+                    nessa construção e foi peça - chave na parte introdutória do IFmath ainda em
+                    autoria-web.</small></p>
+                  <p><b>CURSO: </b> <small>Técnica em informática.</small></p>
+                  <p><b>CONTATO: </b> <small>(84) 9 99404-9806</small></p>
+                     <p><b>E-MAIL: </b> <small>jpjose2600@gmail.com</small></p>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div class="row justify-content-center">
+        <p class="float-center texto text-center">&copy 2021 - Todos os Direitos Reservados | IFMATH</p>
+    </div>
+  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+      $('.collapse').collapse()
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</html>
